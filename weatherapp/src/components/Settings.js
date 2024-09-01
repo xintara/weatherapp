3cd025cc73f6e82b63031305dd27b8abb0fc2b39
xintara <aintara1312@gmail.com>
2024-09-01 13:33:19 -0700

    added selectable measurements and additional information

--- weatherapp/src/components/Settings.js
+++ weatherapp/src/components/Settings.js
@@ -1,8 +1,48 @@
+import { useContext, useState } from "react";
+import '../App.css';
+import WeatherContext from "../context/weather-context";
+import { MEASUREMENT_SYSTEMS } from "../utils/constants";
+
 function Settings() {
+    const { measurementSystem, setMeasurementSystem } = useContext(WeatherContext);
+    const [info, setInfo] = useState(false);
+
+    const changeMeasurementSystem = (system) => {
+        setMeasurementSystem(system);
+    };
+
     return (
-        <div className='Settings'>
-            Settings
-        </div>
+        <>
+            <div className='Settings'>
+                <h4>Measurement Systems:</h4>
+                <div className='measurements'>
+                    {Object.values(MEASUREMENT_SYSTEMS).map((system) => (
+                        <div className={`system ${system === measurementSystem ? 'active' : ''}`}
+                        key={system}
+                        onClick={() => changeMeasurementSystem(system)}
+                        >
+                            {system}
+                        </div>
+                    ))}
+                </div>
+            </div>
+            <div 
+            className='Info'
+            onClick={() => setInfo((prevVal) => !prevVal)}
+            >
+                <i className={`bi bi-info-circle${info ? '-fill' : ''}`}></i>
+                <div className={`infobox ${info ? 'open' : ''}`}>
+                    <div className='description'>
+                        The Product Manager Accelerator Program is designed to support PM professionals through every stage of their career. 
+                        From students looking for entry-level jobs to Directors looking to take on a leadership role, our program has helped over hundreds of students fulfill their career aspirations.
+                        Our Product Manager Accelerator community are ambitious and committed. Through our program they have learnt, honed and developed new PM and leadership skills, giving them a strong foundation for their future endeavours.
+                    </div>
+                    <div className='note'>
+                        This App was made by Alex Intara.
+                    </div>
+                </div>
+            </div>
+        </>
     );
 }
 
--- weatherapp/src/context/weather-context.js
+++ weatherapp/src/context/weather-context.js
@@ -1,5 +1,5 @@
 import {createContext, useEffect, useState} from 'react';
-import { DEFAULT_PLACE, UNITS } from '../utils/constants';
+import { DEFAULT_PLACE, UNITS, MEASUREMENT_SYSTEMS } from '../utils/constants';
 import { getWeatherData } from '../api';
 
 const WeatherContext = createContext();
@@ -11,27 +11,28 @@ function WeatherProvider({children}) {
     const [dailyForecast, setDailyForecast] = useState({});
     const [loading, setLoading] = useState(true);
     const [units, setUnits] = useState({});
+    const [measurementSystem, setMeasurementSystem] = useState(MEASUREMENT_SYSTEMS.AUTO);
 
     useEffect(() => {
         async function _getWeatherData() {
             setLoading(true);
 
-            const weather = await getWeatherData('current', place.place_id, 'auto');
+            const weather = await getWeatherData('current', place.place_id, measurementSystem);
             setCurrentWeather(weather.current);
             setUnits(UNITS[weather.units]);
             
-            const hourlyForecast = await getWeatherData('hourly', place.place_id, 'auto');
+            const hourlyForecast = await getWeatherData('hourly', place.place_id, measurementSystem);
             setHourlyForecast(hourlyForecast.hourly.data);
             setUnits(UNITS[hourlyForecast.units]);
 
-            const dailyForecast = await getWeatherData('daily', place.place_id, 'auto');
+            const dailyForecast = await getWeatherData('daily', place.place_id, measurementSystem);
             setDailyForecast(dailyForecast.daily.data);
             setUnits(UNITS[dailyForecast.units]);
 
             setLoading(false);
         }
         _getWeatherData();
-    }, [place]);
+    }, [place, measurementSystem]);
 
     return (
     <WeatherContext.Provider 
@@ -43,6 +44,8 @@ function WeatherProvider({children}) {
             hourlyForecast,
             dailyForecast,
             units,
+            measurementSystem,
+            setMeasurementSystem
         }}
     >
         {children}
